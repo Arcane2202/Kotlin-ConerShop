2b1c5806347b46418ca8c4b224181a212f1d3bf7
Arcane2202 <tanveershams2218@gmail.com>
2022-02-18 13:50:12 +0600

    CornerShop P{roject
    A project using Kotlin

---
+++ app/src/main/java/project/app/cornershop/MainActivity.kt
@@ -0,0 +1,20 @@
+package project.app.cornershop
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+        val dropdown = findViewById<Spinner>(R.id.dropdownBox)
+
+        val items = arrayOf("1", "2", "three")
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, items)
+
+        dropdown.adapter = adapter
+    }
+}
