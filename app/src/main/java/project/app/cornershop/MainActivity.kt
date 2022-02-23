63052b7b2dc606ad07153b488c7011868041a8b2
Arcane2202 <tanveershams2218@gmail.com>
2022-02-23 21:08:33 +0600

    database connected(start)

--- app/src/main/java/project/app/cornershop/MainActivity.kt
+++ app/src/main/java/project/app/cornershop/MainActivity.kt
@@ -8,13 +8,7 @@ import android.widget.Spinner
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_registration)
-        val dropdown = findViewById<Spinner>(R.id.dropdownBox)
+        setContentView(R.layout.activity_main)
 
-        val items = arrayOf("1", "2", "three")
-
-        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, items)
-
-        dropdown.adapter = adapter
     }
 }
--- app/src/main/java/project/app/cornershop/Registration.kt
+++ app/src/main/java/project/app/cornershop/Registration.kt
@@ -2,10 +2,16 @@ package project.app.cornershop
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 class Registration : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
+        val dropdown = findViewById<Spinner>(R.id.dropdownBox)
+        val items = arrayOf("1", "2", "three")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, items)
+        dropdown.adapter = adapter
     }
 }
