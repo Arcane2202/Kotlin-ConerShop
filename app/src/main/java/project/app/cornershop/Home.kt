e96e600a762136fbdb9463515f129a9d4eed4cd1
Arcane2202 <tanveershams2218@gmail.com>
2022-03-19 20:46:58 +0600

    humm

--- app/src/main/java/project/app/cornershop/Home.kt
+++ app/src/main/java/project/app/cornershop/Home.kt
@@ -8,6 +8,8 @@ import android.view.View
 import android.widget.*
 import androidx.appcompat.widget.SwitchCompat
 import androidx.cardview.widget.CardView
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class Home : Navigation() {
 
@@ -22,12 +24,15 @@ class Home : Navigation() {
     private lateinit var food_snacks : CardView
     private lateinit var others : CardView
 
-    private lateinit var toggleButton : SwitchCompat
+    private lateinit var toggleButton : Button
 
     private lateinit var cartButton:ImageView
     private lateinit var ocrbutton:Button
     private lateinit var notiButton:ImageView
     private lateinit var titleName:TextView
+
+    private lateinit var database:FirebaseDatabase
+    private lateinit var reference: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         if(getSharedPreferences("Shared_Pref", MODE_PRIVATE).getString("Phone",null)==null) {
@@ -35,6 +40,18 @@ class Home : Navigation() {
             val intent = Intent(this@Home, SignIn::class.java)
             startActivity(intent)
         }
+
+        database = FirebaseDatabase.getInstance("https://cornershopmanagement-default-rtdb.asia-southeast1.firebasedatabase.app")
+        reference = database.getReference("Users")
+
+        val phone : String = getSharedPreferences("Shared_Pref", MODE_PRIVATE).getString("Phone",null).toString()
+
+        reference.child(phone).get().addOnSuccessListener{
+            if(it.child("buseiness_ID").value.toString()!="-1") {
+                toggleButton.isEnabled = true;
+            }
+        }
+
         super.onCreate(savedInstanceState)
         val inflater:LayoutInflater = LayoutInflater.from(this)
         val v:View = inflater.inflate(R.layout.activity_home,null,false)
@@ -54,6 +71,24 @@ class Home : Navigation() {
         titleName = findViewById(R.id.titleNav)
         titleName.setText("Home")
 
+
+
+        toggleButton = findViewById(R.id.toggleSellerProf)
+
+        drugs.setOnClickListener{
+            val intent = Intent(this@Home, Drugs::class.java)
+            startActivity(intent)
+        }
+
+        toggleButton.setOnClickListener{
+            val intent = Intent(this@Home, SellerHome::class.java)
+            startActivity(intent)
+        }
+
+        bottomClickListeners()
+    }
+    fun bottomClickListeners() {
+
         cartButton = findViewById(R.id.cartClick)
         ocrbutton = findViewById(R.id.ocrClick)
         notiButton = findViewById(R.id.notiClick)
@@ -70,16 +105,5 @@ class Home : Navigation() {
             val intent = Intent(this@Home, Notifications::class.java)
             startActivity(intent)
         }
-
-        drugs.setOnClickListener{
-            val intent = Intent(this@Home, Drugs::class.java)
-            startActivity(intent)
-        }
-
-        toggleButton.setOnClickListener{
-            val intent = Intent(this@Home, Drugs::class.java)
-            startActivity(intent)
-        }
-
     }
 }
